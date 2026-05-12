015e8e91152dce2784597e18990a4983b2600605
ohahay123321 <jamesmarkasistores@gmail.com>
2026-05-13 06:41:09 +0800

    Catch Throwable instead of PDOException for DB errors

--- config.php
+++ config.php
@@ -22,7 +22,7 @@ function getDB() {
             $db = new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME . ';charset=utf8mb4', DB_USER, DB_PASS);
             $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-        } catch (PDOException $e) {
+        } catch (\Throwable $e) {
             return null;
         }
     }
