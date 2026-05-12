8d7a8b4801f4b951ada3b6f6f61adf02b4a66a2d
ohahay123321 <jamesmarkasistores@gmail.com>
2026-05-13 07:48:03 +0800

    Fix duplicate DB constants in config.php

--- config.php
+++ config.php
@@ -136,8 +136,3 @@ function normalizeEmail($email) {
 
 initDB();
 
-define('DB_HOST', getenv('DB_HOST') ?: 'localhost');
-define('DB_NAME', getenv('DB_NAME') ?: 'moodtracker');
-define('DB_USER', getenv('DB_USER') ?: 'root');
-define('DB_PASS', getenv('DB_PASS') ?: '');
-
