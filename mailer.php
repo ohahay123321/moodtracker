a929900a5f5caa13222895bcbf94334448154ff6
ohahay123321 <jamesmarkasistores@gmail.com>
2026-05-13 04:41:28 +0800

    Support MAIL_SMTP_* env var naming

--- mailer.php
+++ mailer.php
@@ -5,13 +5,17 @@ require_once __DIR__ . '/vendor/autoload.php';
 function getMailer() {
     $mail = new PHPMailer\PHPMailer\PHPMailer(true);
     $mail->isSMTP();
-    $mail->Host = getenv('SMTP_HOST') ?: 'smtp.gmail.com';
+    $mail->Host = getenv('MAIL_SMTP_HOST') ?: getenv('SMTP_HOST') ?: 'smtp.gmail.com';
     $mail->SMTPAuth = true;
-    $mail->Username = getenv('SMTP_USER') ?: '';
-    $mail->Password = getenv('SMTP_PASS') ?: '';
-    $mail->SMTPSecure = PHPMailer\PHPMailer\PHPMailer::ENCRYPTION_STARTTLS;
-    $mail->Port = getenv('SMTP_PORT') ?: 587;
-    $mail->setFrom(getenv('SMTP_FROM_EMAIL') ?: getenv('SMTP_USER') ?: 'noreply@moodtrail.com', getenv('SMTP_FROM_NAME') ?: 'MoodTrail');
+    $mail->Username = getenv('MAIL_SMTP_USER') ?: getenv('SMTP_USER') ?: '';
+    $mail->Password = getenv('MAIL_SMTP_PASS') ?: getenv('SMTP_PASS') ?: '';
+    $enc = getenv('MAIL_SMTP_ENCRYPTION') ?: 'tls';
+    $mail->SMTPSecure = $enc === 'ssl' ? PHPMailer\PHPMailer\PHPMailer::ENCRYPTION_SMTPS : PHPMailer\PHPMailer\PHPMailer::ENCRYPTION_STARTTLS;
+    $mail->Port = getenv('MAIL_SMTP_PORT') ?: getenv('SMTP_PORT') ?: 587;
+    $mail->setFrom(
+        getenv('MAIL_FROM') ?: getenv('SMTP_FROM_EMAIL') ?: getenv('MAIL_SMTP_USER') ?: getenv('SMTP_USER') ?: 'noreply@moodtrail.com',
+        getenv('MAIL_FROM_NAME') ?: getenv('SMTP_FROM_NAME') ?: 'MoodTrail'
+    );
     $mail->isHTML(true);
     return $mail;
 }
